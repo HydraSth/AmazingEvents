ceced3d454ab54086fe50cab9fcd322fa87f3484
Juan Cruz <juanespasan@gmail.com>
2023-03-23 17:05:51 -0300

    Optimizado el filtro por texto

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-fetch('../assets/amazing.json')
-  .then(response => response.json())
-  .then(data => agarrarData(data))
-  .catch(error => console.error(error));
+import {data} from "./data.js"
+let objeto = data;
 
+const bodyInsert = document.getElementById("bodyInsert")
 function agarrarData(data){
-    if(window.location.href.includes('/index.html')){
+    if(window.location.href.includes('/index.html')||window.location.href.includes('')){
         data.events.forEach((element)=>InsertarElementos(element))
     }else if(window.location.href.includes('/pEvents.html')){
         data.events.forEach((element)=>{ if(element.date<data.currentDate){InsertarElementos(element)} })
@@ -12,9 +11,8 @@ function agarrarData(data){
         data.events.forEach((element)=>{ if(element.date>data.currentDate){InsertarElementos(element)} })
     }
 }
+agarrarData(objeto);
 
-
-const bodyInsert = document.getElementById("bodyInsert")
 //Insercion de elementos
 function InsertarElementos(element){
     if (bodyInsert != null) {
@@ -44,104 +42,120 @@ function InsertarElementos(element){
 	}
 }
 
+// Crea filtros
+function crearFiltros(json){
+    let seccionFiltros = document.getElementById("seccion-filtros");
+    let filtros = [];
+    json.events.forEach(element => {
+        let filtro = element.category
+        if(!filtros.includes(element.category)){
+            seccionFiltros.insertAdjacentHTML('beforeend', 
+            `<li>
+            <input class="filtro" type="checkbox" data-valor="${filtro}" id="${filtro}">
+            <label class="pe-3" for="${filtro}">${filtro}</label>
+            </li>
+            `)
+            filtros.push(filtro)
+        }
+    });
+}
+crearFiltros(objeto)
+let casillasFiltros = Array.from(document.querySelectorAll(".filtro"))
 
-// // Crea filtros
-// function crearFiltros(json){
-//     let seccionFiltros = document.getElementById("seccion-filtros");
-//     let filtros = [];
-//     json.events.forEach(element => {
-//         let filtro = element.category
-//         if(!filtros.includes(element.category)){
-//             seccionFiltros.insertAdjacentHTML('beforeend', 
-//             `<li>
-//             <input class="filtro" type="checkbox" data-valor="${filtro}" id="${filtro}">
-//             <label class="pe-3" for="${filtro}">${filtro}</label>
-//             </li>
-//             `)
-//             filtros.push(filtro)
-//         }
-//     });
-// }
-// crearFiltros(data)
+//Filtrado
+const Tarjetas = Array.from(document.querySelectorAll(".card"))
+casillasFiltros.forEach(filtro=>filtro.addEventListener("change", filtradoTotal))
 
-// //Filtrado
-// const Tarjetas = Array.from(document.querySelectorAll(".card"))
-// const filtros = Array.from(document.querySelectorAll(".filtro"))
-// //Listener de evento
-// filtros.forEach((filtro) => {
-//     filtro.addEventListener("change", filtradoTotal)
-// })
+function filtradoTotal(){
+    let filtroAproved= filtrarCheckbox()
+    let idValidas=obetenerIds(filtroAproved);
+    imprimirTarjetas(idValidas)
+}
 
-// function filtradoTotal(){
-//     let filtroAproved= filtrarCheckbox()
-//     tarjetaValida(filtroAproved)
-// }
+function filtrarCheckbox(){
+    let filtrosActivos=casillasFiltros.filter(filtro=>filtro.checked==true).map((filtro)=>{return filtro.id});
+    return Tarjetas.filter((tarjeta)=>{
+        let categoriasTarjeta=tarjeta.classList.value;
+        return filtrosActivos.some((filtro)=>{
+            return categoriasTarjeta.includes(filtro);
+        }); 
+    });
+}
 
-// function filtrarCheckbox(){
-//     let filtrosActivos=filtros.filter(filtro=>filtro.checked==true).map((filtro)=>{return filtro.id});
-//     return Tarjetas.map((tarjeta)=>filtrosActivos.some(filtro=>tarjeta.classList.contains(filtro))? tarjeta:undefined).filter(element=> element!=undefined);
-// }
+function obetenerIds(elementosValidos){
+    return elementosValidos.map(elemento=>{
+        let clases=elemento.classList;
+        let id=clases[clases.length - 1];
+        return id;
+    })
+}
 
-// function tarjetaValida(TarjetasValidas){
-//     Tarjetas.forEach(tarjeta=>tarjeta.classList.add('d-none'))
-//     console.log(TarjetasValidas)
-//     TarjetasValidas.forEach((element)=>InsertarElementos(element))
-// }
+function imprimirTarjetas(ids){
+    if(ids.length>0){
+        bodyInsert.innerHTML=''
+        ids.forEach((id)=>{
+            const objetosBuscados = data.events.filter(evento => id.includes(evento._id));
+            InsertarElementos(objetosBuscados[0])
+        })
+    }else{
+        agarrarData(objeto)
+    }
+}
 
 // Filtrado de texto
-// const search = document.querySelector("#Search")
+const search = document.querySelector("#Search")
+search.addEventListener("keyup", filtradoTotal)
 
-// //Listener de texto
-// search.addEventListener("keyup", ()=>{
-//     //Cantidad de tarjetas ocultas
-//     let tarjetasOcultas = 0
-//     //Si la barra de busqueda no esta vacia
-//     if(search.value!=''){
-//         //Si no hay filtros activos
-//         if(filtrosActivos.length==0){
-//             Tarjetas.forEach((Tarjeta)=>{
-//                 //Se guarda toda la informacion de la tarjeta
-//                 let ClasesTarjeta=Tarjeta.textContent.toLowerCase()
-//                 if(ClasesTarjeta.includes(search.value.toLowerCase())){
-//                     visibility(Tarjeta,'mostrar')
-//                     notFound(false)
-//                 }else{
-//                     visibility(Tarjeta,'ocultar')
-//                     tarjetasOcultas +=1
-//                 }
-//                 if(tarjetasOcultas==Tarjetas.length){
-//                     notFound(true)
-//                 }
-//         })
-//         //Si hay filtros activos
-//         }else{
-//             let Activos = document.querySelectorAll('.Activo')
-//             Activos.forEach((Tarjeta)=>{
-//                 //Se guarda toda la informacion de la tarjeta
-//                 let ClasesTarjeta=Tarjeta.textContent.toLowerCase()
-//                 if(ClasesTarjeta.includes(search.value.toLowerCase())){
-//                     visibility(Tarjeta,'mostrar')
-//                     notFound(false)
-//                 }else{
-//                     visibility(Tarjeta,'ocultar')
-//                     tarjetasOcultas +=1
-//                 }
+function filtradoTexto(){
+    //Cantidad de tarjetas ocultas
+    let tarjetasOcultas = 0
+    //Si la barra de busqueda no esta vacia
+    if(search.value!=''){
+        //Si no hay filtros activos
+        if(filtrosActivos.length==0){
+            Tarjetas.forEach((Tarjeta)=>{
+                //Se guarda toda la informacion de la tarjeta
+                let ClasesTarjeta=Tarjeta.textContent.toLowerCase()
+                if(ClasesTarjeta.includes(search.value.toLowerCase())){
+                    visibility(Tarjeta,'mostrar')
+                    notFound(false)
+                }else{
+                    visibility(Tarjeta,'ocultar')
+                    tarjetasOcultas +=1
+                }
+                if(tarjetasOcultas==Tarjetas.length){
+                    notFound(true)
+                }
+        })
+        //Si hay filtros activos
+        }else{
+            let Activos = document.querySelectorAll('.Activo')
+            Activos.forEach((Tarjeta)=>{
+                //Se guarda toda la informacion de la tarjeta
+                let ClasesTarjeta=Tarjeta.textContent.toLowerCase()
+                if(ClasesTarjeta.includes(search.value.toLowerCase())){
+                    visibility(Tarjeta,'mostrar')
+                    notFound(false)
+                }else{
+                    visibility(Tarjeta,'ocultar')
+                    tarjetasOcultas +=1
+                }
 
-//                 if(tarjetasOcultas==Activos.length){
-//                     notFound(true)
-//                 }
-//             })
-//         }
-//     //Si la barra de busqueda esta vacia
-//     }else{
-//         if(filtrosActivos.length==0){
-//             Tarjetas.forEach((Tarjeta)=>{
-//                 visibility(Tarjeta,'mostrar')
-//                 notFound(false)
-//             })
-//         }
-//     }
-// })
+                if(tarjetasOcultas==Activos.length){
+                    notFound(true)
+                }
+            })
+        }
+    //Si la barra de busqueda esta vacia
+    }else{
+        if(filtrosActivos.length==0){
+            Tarjetas.forEach((Tarjeta)=>{
+                visibility(Tarjeta,'mostrar')
+                notFound(false)
+            })
+        }
+    }
+}
 
 // //Si no encuentra los elementos..
 // function notFound(cantidadDeCarteles){
@@ -164,14 +178,5 @@ function InsertarElementos(element){
 //             cartel.remove()
 //             cantidadDeCarteles=0
 //         }
-//     }
-// }
-
-// //Oculta o muestra los elementos
-// function visibility(elemento,view){
-//     if(view=='mostrar'){
-//         elemento.style.setProperty("display", "block", "important")
-//     }else if(view=='ocultar'){
-//         elemento.style.setProperty("display", "none", "important")
 //     }
 // }
